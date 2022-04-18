7ecba98f87a6113790ea28b09f61f49dcd1db501
rodentskie <rodentskie@gmail.com>
2022-04-18 08:08:45 +0800

    chore: fix docker files for session

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
-FROM node:14.17.5
+FROM node:14-alpine
 WORKDIR /usr/src/app
 COPY package*.json ./
 
-RUN npm install
+RUN apk update
+RUN npm ci
 
 COPY . .
 
 RUN npm run build
-
+ARG BUILD_PORT
+ENV PORT=${BUILD_PORT}
 EXPOSE ${PORT}
 
 CMD ["npm", "start"]
--- data/index.ts
+++ data/index.ts
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const dbConn = () => {
-  const uri: string = process.env.MONGO_URI || 'mongodb://localhost:27017/onboarding_graphql_test?authSource=admin';
-
+  const uri: string = process.env.MONGO_URI || 'mongodb://localhost:27017/onboarding_graphql_test';
+  
   const db = connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
